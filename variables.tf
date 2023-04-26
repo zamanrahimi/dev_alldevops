e5a9ae8507f72b7213b11fb922ab25aee2f84364
zamanrahimi <zaman.rahimi1368@gmail.com>
2023-04-26 15:22:48 -0500

    first commit

--- variables.tf
+++ variables.tf
@@ -1,19 +1,21 @@
 # zaman2
-provider "aws" {
-  region     = "us-east-1"
-  access_key = "AKIAXLW2ED6IHJ7A2HMP"
-  secret_key = "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"
-}
 
-# change allways 
-variable "keyname" {
-    type = string
-    default = "ec2_key"
-}
-
-# push changes to dev repository in github
-resource "null_resource" "run_script" {
-  provisioner "local-exec" {
-    command = "./dev_alldevops.bat"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
   }
 }
+
+provider "aws" {
+  region     = "us-east-1"
+  # access_key = "AKIAXLW2ED6IHJ7A2HMP"
+  # secret_key = "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"
+  access_key = "AKIASIUNHVMNDDBRTA62"
+  secret_key = "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"
+
+}
+
+
