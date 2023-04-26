2d6577dc318d9255d0162272f3743b3e02d389fd
zamanrahimi <zaman.rahimi1368@gmail.com>
2023-04-26 11:33:50 -0500

    first commit

---
+++ variables.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIAXLW2ED6IHJ7A2HMP"
+  secret_key = "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"
+}
+
+# change allways 
+variable "keyname" {
+    type = string
+    default = "ec2_key"
+}
+
+# push changes to dev repository in github
+resource "null_resource" "run_script" {
+  provisioner "local-exec" {
+    command = "./example.sh"
+  }
+}
