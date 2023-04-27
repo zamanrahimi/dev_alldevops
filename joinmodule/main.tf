b6c976d3dcac93550f274be4e94b0a5429cf3823
zamanrahimi <zaman.rahimi1368@gmail.com>
2023-04-26 22:19:04 -0500

    zrahimi iam user is added

--- joinmodule/main.tf
+++ joinmodule/main.tf
@@ -1,3 +1,4 @@
 # Register your module here !
 module "vpc" { source  = "../module/vpc" }
 module "ec2" { source  = "../module/ec2"}
+module "iam" { source  = "../module/iam"}
---
+++ module/iam/zrahimi.tf
@@ -0,0 +1,28 @@
+# add your code here
+
+resource "aws_iam_user" "zrahimi" {
+  name = "zrahimi"
+  path = "/system/"
+
+  tags = {
+    name = "zrahimi"
+  }
+}
+
+resource "aws_iam_access_key" "access_key_zaman" {
+  user = aws_iam_user.zrahimi.name
+}
+
+data "aws_iam_policy_document" "policy_name" {
+  statement {
+    effect    = "Allow"
+    actions   = ["*"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_user_policy" "policy_for_zrahimi" {
+  name   = "policy_for_zrahimi"
+  user   = aws_iam_user.zrahimi.name
+  policy = data.aws_iam_policy_document.policy_for_zrahimi.json
+}
