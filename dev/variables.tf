73e3c06848c44384a66c1b53cfa1207628323fbf
zamanrahimi <zaman.rahimi1368@gmail.com>
2023-04-26 20:06:18 -0500

    first commit1

--- dev/variables.tf
+++ dev/variables.tf
@@ -11,10 +11,8 @@ terraform {
 
 provider "aws" {
   region     = "us-east-1"
-  # access_key = "AKIAXLW2ED6IHJ7A2HMP"
-  # secret_key = "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"
-  access_key = "AKIASIUNHVMNDDBRTA62"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 
 }
 
